796a50fcc53affebe8f8a48e8a33d37dcd7e1a50
SeoliKim <108114194+SeoliKim@users.noreply.github.com>
2023-10-08 10:52:21 -0400

    Rewrite All

---
+++ assets/js/imageSearch.js
@@ -0,0 +1,9 @@
+const accessKey = "KVin7mMt37a2S6U-BKL3w8elEJYz2pjupQE8_nNDLJo"
+
+let inputData= ""
+let page =1;
+
+function searchImages(){
+	const url = "https://api.unsplash.com/search/photos=${page}&query=${inputData}"
+
+}
