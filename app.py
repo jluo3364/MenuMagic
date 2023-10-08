52b24aaad62ffb25871c2b8c2764791e3393efc1
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2023-10-07 20:50:21 -0400

    filtering options

--- app.py
+++ app.py
@@ -2,26 +2,36 @@ from flask import Flask, render_template
 import sqlite3
 import csv
 import db
-from db import openDB
+from db import *
 
 app = Flask(__name__)
 
 conn, cur = openDB()
 cur.execute("CREATE TABLE IF NOT EXISTS allitems (id INTEGER PRIMARY KEY AUTOINCREMENT, food_name TEXT NOT NULL, food_type TEXT NOT NULL, meal TEXT NOT NULL, dining_location TEXT NOT NULL )")
-with open('diningScraper.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader)  # Skip the header row if it exists
+# with open('diningScraper.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     next(csv_reader)  # Skip the header row if it exists
 
-    for row in csv_reader:
-        cur.execute('INSERT INTO allitems (food_name, food_type, meal, dining_location) VALUES (?,?,?, ?)', (row[0], row[1], row[2], row[3]))
-conn.commit()
+#     for row in csv_reader:
+#         cur.execute('INSERT INTO allitems (food_name, food_type, meal, dining_location) VALUES (?,?,?, ?)', (row[0], row[1], row[2], row[3]))
+# conn.commit()
+close(conn, cur)
+
+atriumItems = getLocationItems("Atrium")
+buschItems = getLocationItems("Busch")
+liviItems = getLocationItems("Livingston")
+neilsonItems = getLocationItems("Neilson")
 
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/busch.html")
 def busch():
+    print(filterByMealAndLocation("Lunch", "Busch"))
     return render_template("busch.html")
 
 @app.route("/livingston.html")
@@ -31,3 +41,10 @@ def livingston():
 @app.route("/neilson.html")
 def neilson():
     return render_template("neilson.html")
+
+@app.route("/atrium.html")
+def atrium():
+    return render_template("atrium.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -3,3 +3,43 @@ import sqlite3
 def openDB():
     conn = sqlite3.connect('static/menus.db')
     return (conn, conn.cursor())
+
+def close(conn, cur):
+    cur.close()
+    conn.close()
+
+def getLocationItems(location): #locations are Atrium, Busch, Livingston, Neilson, 
+    conn, cur = openDB()
+
+    sql = "SELECT * FROM allitems WHERE dining_location = ?"
+    cur.execute(sql, (location, ))
+    res = cur.fetchall() #get rows
+    items = []
+    for r in res:
+        items.append(r)
+    close(conn, cur)
+    return items
+
+def filterByMeal(meal):
+    conn, cur = openDB()
+
+    sql = "SELECT * FROM allitems WHERE meal = ?"
+    cur.execute(sql, (meal, ))
+    res = cur.fetchall() #get rows
+    items = []
+    for r in res:
+        items.append(r)
+    close(conn, cur)
+    return items
+
+def filterByMealAndLocation(meal, location):
+    conn, cur = openDB()
+
+    sql = "SELECT * FROM allitems WHERE dining_location = ? AND meal = ?"
+    cur.execute(sql, (location, meal))
+    res = cur.fetchall() #get rows
+    items = []
+    for r in res:
+        items.append(r)
+    close(conn, cur)
+    return items
