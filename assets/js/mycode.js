aff6f6fa5a8793b1851db3a81bd3893dce5d79b2
SeoliKim <108114194+SeoliKim@users.noreply.github.com>
2023-10-08 13:00:16 -0400

    push

--- assets/js/mycode.js
+++ assets/js/mycode.js
@@ -1,18 +1,6 @@
-const fooditems = document.querySelectorAll('.food .item');
-fooditems.forEach((fooditem) => {
-    fooditem.addEventListener('mouseover', function (event) {
-        var foodfront = fooditem.querySelector('.front');
-        var foodback = fooditem.querySelector('.back');
-        foodback.classList.remove('hidden');
-        foodfront.classList.add('hidden');
-    });
-    fooditem.addEventListener('mouseleave', function (event) {
-        var foodfront = fooditem.querySelector(".front");
-        var foodback = fooditem.querySelector(".back");
-        foodback.classList.add('hidden');
-        foodfront.classList.remove('hidden');
-    });
-  });
+
+
+
 
 
  
